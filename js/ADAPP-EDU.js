b43865f902fd7f905c697a36d0831200b65df1b8
hansschaa <shadow360x@gmail.com>
2017-10-12 13:08:41 -0300

    posible solucion 15

--- js/ADAPP-EDU.js
+++ js/ADAPP-EDU.js
@@ -36,12 +36,12 @@ $(document).ready(function(){
     {
 
         //ESTILOS CELULAR
-        $("#answer-container-list li").css('padding-top', 'calc(100vh/38)');
-        $("#answer-container-list li").css('padding-bottom', 'calc(100vh/38)');
+        $("#answer-container-list li").css('padding-top', 'calc(100vh/10)');
+        $("#answer-container-list li").css('padding-bottom', 'calc(100vh/10)');
         $("#answer-container-list li").css('font-size', '3vh');
         $("#answer-container-list li").css('margin-top', 'calc(100vh/120)');
         $("#answer-container-list li").css('margin-bottom', 'calc(100vh/120)');
-        $(".answer").css('padding-top','1%');
+        $(".answer").css('padding','1%');
 
         isUsingDesktop = false;
         //$(".container").height(window.innerHeight);		
