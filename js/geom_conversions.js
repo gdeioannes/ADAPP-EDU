474e9cab9a1d399901d2437dca4171082e51c5b8
hansschaa <shadow360x@gmail.com>
2017-11-10 01:28:19 -0300

    junte las funciones en utilities y borre el archivo degrees

--- js/geom_conversions.js
+++
@@ -1,3 +0,0 @@
-function degreeToRadians(value){
-	return (value/360)*2*Math.PI;
-}
--- js/utilities.js
+++ js/utilities.js
@@ -6,3 +6,7 @@ function maxSizeText(maxLength,text,addTextEnd)
         return text;
     }
 }
+
+function degreeToRadians(value){
+	return (value/360)*2*Math.PI;
+}
